5418c62dea4f8f6fe7a7f492c2daa4eeaa72cc9f
Vaibhav Pal <vaibhavpal.info@gmail.com>
2026-01-08 00:36:31 +0530

    first commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+  //react fragmentation
+  <>
+  <Navbar />
+  </>
+  );
+}
+
+export default App;
